f83adf5802e336a6d5eb540a61d4e23a3de553f1
Heng Xiao <hengxiao80@cirrus-login1.ccs.ornl.gov>
2025-04-19 01:41:14 -0400

    Added call to radar_init in micro_init() for HUJISBM.

--- SRC/MICRO_HUJISBM/microphysics.f90
+++ SRC/MICRO_HUJISBM/microphysics.f90
@@ -2125,7 +2125,6 @@ end subroutine micro_print
      qv_old= qv
   end if
 
-
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ! variables for radar reflecitivity calculations
 !..Create bins of rain (from min diameter up to 5 mm).
@@ -2167,6 +2166,8 @@ end subroutine micro_print
   do i = 1, 256
      mp_debug(i:i) = char(0)
   enddo
+
+  call radar_init
 
         return
       END SUBROUTINE micro_init
